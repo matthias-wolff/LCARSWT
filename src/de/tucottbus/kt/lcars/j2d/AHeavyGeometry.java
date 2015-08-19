af47c6ec7311c39af85e77ab6890a89598f52ea9
birthmar <birthmar@tu-cottbus.de>
2015-08-19 15:09:35 +0200

    o [changed] initialization

--- src/de/tucottbus/kt/lcars/j2d/AHeavyGeometry.java
+++ src/de/tucottbus/kt/lcars/j2d/AHeavyGeometry.java
@@ -1,8 +1,9 @@
 package de.tucottbus.kt.lcars.j2d;
 
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,7 +18,7 @@ public abstract class AHeavyGeometry extends Geometry
 
   private static final AtomicLong currentSerialNo = new AtomicLong();
 
-  public final long serialNo;
+  public final long _serialNo;
 
   private int _x;
   private int _y;
@@ -27,23 +28,37 @@ public abstract class AHeavyGeometry extends Geometry
   public AHeavyGeometry(Point position, Dimension size, boolean foreground)
   {
     super(foreground);
-    this.serialNo = currentSerialNo.incrementAndGet();
+    this._serialNo = currentSerialNo.incrementAndGet();
+    _x = position.x;
+    _y = position.y;
+    _width = size.width;
+    _height = size.height;
+  }
+  
+  public AHeavyGeometry(Rectangle bounds, boolean foreground)
+  {
+    super(foreground);
+    this._serialNo = currentSerialNo.incrementAndGet();
+    _x = bounds.x;
+    _y = bounds.y;
+    _width = bounds.width;
+    _height = bounds.height;
   }
 
   public void onAddToScreen()
   {
-    if(workerList.containsKey(serialNo))
+    if(workerList.containsKey(_serialNo))
       return;
         
     // create worker and add to worker list
-    workerList.put(serialNo,
+    workerList.put(_serialNo,
         new HeavyRenderWorker(new Dimension(_width, _height),
-            (input, img) -> paint2DAsync(input, img)));
+            (input, img) -> paint2DAsync(input, img.createGraphics())));
   }
 
   public void onRemoveFromScreen()
   {
-    workerList.remove(serialNo);
+    workerList.remove(_serialNo);
   }
 
   /**
@@ -51,12 +66,12 @@ public abstract class AHeavyGeometry extends Geometry
    * @param input
    * @param image
    */
-  protected abstract void paint2DAsync(Object input, BufferedImage image);
+  protected abstract void paint2DAsync(Object input, Graphics2D image);
 
   @Override
   public void paint2D(AdvGraphics2D g2d)
   {
-    HeavyRenderWorker worker = workerList.get(serialNo);
+    HeavyRenderWorker worker = workerList.get(_serialNo);
     if (worker == null)
       throw new NullPointerException("missing worker in worker list");
     g2d.drawImage(worker.GetImage(), _x, _y, null); // TODO: ImageObserver?
