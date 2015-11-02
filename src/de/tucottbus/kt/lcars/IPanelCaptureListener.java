f1eb7e56d2f92078592c53b91bd7f96c15d212af
Borck <borck@hotmail.de>
2015-11-02 16:54:56 +0100

    minor changes

--- src/de/tucottbus/kt/lcars/IPanelCaptureListener.java
+++ src/de/tucottbus/kt/lcars/IPanelCaptureListener.java
@@ -1,9 +1,10 @@
 package de.tucottbus.kt.lcars;
 
 import java.awt.image.BufferedImage;
+import java.rmi.Remote;
 import java.util.concurrent.FutureTask;
 
-public interface IPanelCaptureListener
+public interface IPanelCaptureListener extends Remote
 {
 	void onScreenUpdate(FutureTask<BufferedImage> screenCapture);
 }
