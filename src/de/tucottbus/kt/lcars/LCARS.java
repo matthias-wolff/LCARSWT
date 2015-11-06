642800ff6ca99b8162e50e70e9b96311a1b3afb3
birthmar <birthmar@tu-cottbus.de>
2015-11-06 14:21:54 +0100

    fixed synchronization issue in LCARS.getFontMeta(int), minor change

--- src/de/tucottbus/kt/lcars/LCARS.java
+++ src/de/tucottbus/kt/lcars/LCARS.java
@@ -516,7 +516,7 @@ public class LCARS implements ILcarsRemote
       
       int    h = 1200;/*LCARS.panelDim.height;*/
       
-      fonts = new FontMeta.Explicit[EF_COUNT];
+      FontMeta.Explicit[] fonts = new FontMeta.Explicit[EF_COUNT];
       final Function<Integer, FontMeta.Explicit> newFont = (height) -> {       
         return new FontMeta.Explicit(f[0], (int)(height*fontScale), java.awt.Font.PLAIN);
       };
@@ -532,7 +532,7 @@ public class LCARS implements ILcarsRemote
       fonts[EF_NORMAL >>EF_SHIFT] = newFont.apply((int)(h/37.5));
       fonts[EF_SMALL >>EF_SHIFT] = newFont.apply((int)(h/50.0));
       fonts[EF_TINY >>EF_SHIFT] = newFont.apply((int)(h/65.0));                                
-      
+      LCARS.fonts = fonts;
     }
     int font = (style & ES_FONT) >> EF_SHIFT;
     if (font<0 || font>=fonts.length) return fonts[0];
@@ -1022,6 +1022,7 @@ public class LCARS implements ILcarsRemote
    * @return the contents
    * @deprecated Use {@link #loadTextResource(String)} instead!
    */
+  @Deprecated
   public static String loadTextFile(File file) throws FileNotFoundException
   {
     if (file==null) return null;
@@ -1610,6 +1611,7 @@ public class LCARS implements ILcarsRemote
       // Install shut-down hook
       Runtime.getRuntime().addShutdownHook(new Thread()
       {
+        @Override
         public void run()
         {
           Log.info("Shutting down ...");
